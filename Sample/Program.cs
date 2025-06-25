ff750ac71b8299bf102da6ed6a90ff5030d39458
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2025-06-25 12:55:25 +0400

    Initial commit

---
+++ Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.DashboardCommon;
+
+namespace DashboardCICDExport {
+    internal class Program {
+        static void Main(string[] args) {
+            var exp = new DashboardExporter(); 
+
+            exp.DataLoading += Exp_DataLoading;
+
+            exp.ExportToImage(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\dashboard1.xml"), "output.png");
+
+            Console.WriteLine("Ready");
+            Console.ReadLine();
+        }
+
+        private static void Exp_DataLoading(object sender, DataLoadingEventArgs e) {
+            e.Data = ProductSales.GetProductSales();
+        }
+    }
+}
